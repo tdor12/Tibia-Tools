2fa8f5a3f29badf5bc31dfb177d0caea52f0379d
imora128 <imora128@students.bju.edu>
2018-03-16 13:37:27 -0400

    test

---
+++ Tibia Tools/Tibia Tools/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using Tibia_Tools.Properties;
+using System.Xml.Linq;
+
+namespace Tibia_Tools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         *Date: MM/DD/YYYY
+         * HuntName:
+         * Desc:
+         * Type: Solo, Duo, Trio, 4man
+         * Duration:
+         * Profit:
+         * TotalExp:
+         * MemberNames:
+         * 
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("HuntLog.xml"))
+            {
+
+                MessageBox.Show("HuntLog.xml FOUND!!");
+                XDocument doc = Singleton.Instance.GetXML();
+                doc = XDocument.Load("HuntLog.XML");
+                Singleton.Instance.SetXML(doc);
+
+                XElement hunt = doc.Descendants("HuntLog").FirstOrDefault();
+                if (hunt.HasElements) {
+                var eles = hunt.Elements().ToList();
+                //setting the id to the last ID in the xml when prog loads up
+                Singleton.Instance.SetID(Convert.ToInt32(eles[eles.Count - 1].FirstAttribute.Value));
+                List<String> myList = new List<string> { "Italo The Tank(EK 320)", "Italo The ED (321 ED)" };
+                Singleton.Instance.AddHunt("Draken Walls Test", "3/20/2018", "Testing, bro", "Duo", "3", "100", "123456", myList);
+            }
+                Singleton.Instance.PrintEles();
+                
+
+
+            } else
+            {
+                MessageBox.Show("HuntLog.xml not found, creating one.");
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.NewLineOnAttributes = true;
+
+                using (XmlWriter writer = XmlWriter.Create("HuntLog.xml", settings)) 
+                {
+                    Singleton.Instance.IncrementID();
+                    writer.WriteStartDocument();
+                    //huntlog begin
+                    writer.WriteStartElement("HuntLog");
+                    
+                    //hunt begin
+                    writer.WriteStartElement("Hunt");
+                    writer.WriteAttributeString("ID", "0");
+                    writer.WriteElementString("Name", "Draken Walls Test");
+                    writer.WriteElementString("Date", "3/16/2018");
+                    writer.WriteElementString("Desc", "Testing out to see if this works");
+                    writer.WriteElementString("Type", "Solo");
+                    writer.WriteElementString("Duration", "0");
+                    writer.WriteElementString("Profit", "0");
+                    writer.WriteElementString("EXP", "0");
+
+                    //members begin
+                    writer.WriteStartElement("Members");
+                    writer.WriteElementString("Name", "Italo The Tank");
+                    //members end
+                    writer.WriteEndElement();
+                    //hunt end
+                    writer.WriteEndElement();
+
+                    //huntlog end
+                    writer.WriteEndElement();
+                    writer.Flush();
+                    writer.Close();
+                }
+
+
+
+
+            }
+        }
+    }
+}
---
+++ Tibia Tools/Tibia Tools/Properties/Singleton.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Tibia_Tools.Properties
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private XDocument doc = new XDocument();
+        private int id;
+
+
+        private Singleton() {; }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public void SetXML(XDocument _writer)
+        {
+            this.doc = _writer;
+        }
+
+        public XDocument GetXML()
+        {
+            return this.doc;
+        }
+        public void IncrementID()
+        {
+            this.id = id + 1;
+        }
+
+        public int GetID()
+        {
+            return this.id;
+        }
+
+        public void SetID(int id)
+        {
+            this.id = id;
+        }
+
+        public void PrintEles()
+        {
+            XElement hunt = doc.Descendants("HuntLog").FirstOrDefault();
+            var eles = hunt.Elements().ToList();
+            Console.WriteLine(eles[eles.Count - 1].FirstAttribute.Value);
+        }
+
+        public void AddHunt(String huntName, String huntDate, String huntDesc, String huntType, String huntDuration, String huntProfit, String huntEXP, List<String> memberList)
+        {
+            List<XElement> membersList = new List<XElement>();
+            memberList.ForEach(delegate (String _name) {
+                membersList.Add(new XElement("Name", _name));
+
+            });
+            Console.WriteLine("Inside addhunt");
+            Console.WriteLine(id);
+            XElement hunt = doc.Descendants("HuntLog").FirstOrDefault();
+            if (hunt != null)
+            {
+                Console.WriteLine("Inside hunt != null");
+                IncrementID();
+                XElement name = new XElement("Name", huntName);
+                XElement date = new XElement("Date", huntDate);
+                XElement desc = new XElement("Desc", huntDesc);
+                XElement type = new XElement("Type", huntType);
+                XElement duration = new XElement("Duration", huntDuration);
+                XElement profit = new XElement("Profit", huntProfit);
+                XElement EXP = new XElement("EXP", huntEXP);
+                XElement members = new XElement("Members", membersList[0]);
+                hunt.Add(new XElement("Hunt", new XAttribute("ID", id.ToString()),
+                    name, date, desc, type, duration, profit, EXP, members));
+                XElement test = doc.Descendants("Hunt").Where(x => (string)x.Attribute("ID") == id.ToString()).FirstOrDefault();
+                IEnumerable<XElement> rows = test.Descendants("Members");
+                XElement firstRow = rows.First();
+                for (int i = 1; i < membersList.Count; i++)
+                {
+                    members.Add(new XElement(membersList[i]));
+                }
+                doc.Save("HuntLog.xml");
+            }
+        }
+    }
+}
